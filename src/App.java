08fe2cfca463ca7a12d4745b9fc4e5381a8c7a3c
PROFESSOR M35 <professorm35@acad.unifor.br>
2024-05-22 09:15:04 -0300

    Padrão prototype

--- src/App.java
+++ src/App.java
@@ -1,37 +1,56 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Jogador jogador1 = new Jogador("Jog 1");
-        Jogador jogador2 = new Jogador("Jog 2");
-        Jogador jogador3 = new Jogador("Jog 3");
+        Time novo1 = new Time("Ferroviario");
+        Time novo2 = novo1.clone();
+        Time novo3 = novo1.clone();
+        Time novo4 = novo1.clone();
+        Time novo5 = novo1.clone();
         
-        Jogador jogador4 = new Jogador("Jog 4");
-        Jogador jogador5 = new Jogador("Jog 5");
-        Jogador jogador6 = new Jogador("Jog 6");
-
-        Time ferroviario = new Time("Ferroviario");
-        ferroviario.adicionarJogador(jogador1);
-        ferroviario.adicionarJogador(jogador2);
-        ferroviario.adicionarJogador(jogador3);
+        novo1.setCorCamisa("Verde e Lilás");
+        novo1.getJogadores().get(0).setNome("Tsubasa");
+        novo1.adicionarJogador(new Jogador("Ninja Jiraya"));
         
-        Time sobral = new Time("Sobral");
-        sobral.adicionarJogador(jogador4);
-        sobral.adicionarJogador(jogador5);
-        sobral.adicionarJogador(jogador6);
+        System.out.println(novo1);
+        System.out.println(novo2);
+        System.out.println(novo3);
+        System.out.println(novo4);
+        System.out.println(novo5);
 
-        Espectador davi = new Espectador("Davi");
-        Espectador rafael = new Espectador("Rafael");
 
-        Partida classico = new Partida(ferroviario, sobral);
-        classico.inscrever(davi);
-        classico.inscrever(rafael);
 
-        classico.marcarGol(ferroviario, jogador2);
-        classico.marcarGol(ferroviario, jogador1);
-        classico.marcarGol(ferroviario, jogador3);
-        classico.marcarGol(ferroviario, jogador3);
-        classico.marcarGol(sobral, jogador4);
-        classico.marcarGol(sobral, jogador6);
-        classico.marcarGol(sobral, jogador5);
-        classico.gerarResultados();
+
+
+        // Jogador jogador1 = new Jogador("Jog 1");
+        // Jogador jogador2 = new Jogador("Jog 2");
+        // Jogador jogador3 = new Jogador("Jog 3");
+        // Jogador jogador4 = new Jogador("Jog 4");
+        // Jogador jogador5 = new Jogador("Jog 5");
+        // Jogador jogador6 = new Jogador("Jog 6");
+
+        // Time ferroviario = new Time("Ferroviario");
+        // ferroviario.adicionarJogador(jogador1);
+        // ferroviario.adicionarJogador(jogador2);
+        // ferroviario.adicionarJogador(jogador3);
+        
+        // Time sobral = new Time("Sobral");
+        // sobral.adicionarJogador(jogador4);
+        // sobral.adicionarJogador(jogador5);
+        // sobral.adicionarJogador(jogador6);
+
+        // Espectador davi = new Espectador("Davi");
+        // Espectador rafael = new Espectador("Rafael");
+
+        // Partida classico = new Partida(ferroviario, sobral);
+        // classico.inscrever(davi);
+        // classico.inscrever(rafael);
+
+        // classico.marcarGol(ferroviario, jogador2);
+        // classico.marcarGol(ferroviario, jogador1);
+        // classico.marcarGol(ferroviario, jogador3);
+        // classico.marcarGol(ferroviario, jogador3);
+        // classico.marcarGol(sobral, jogador4);
+        // classico.marcarGol(sobral, jogador6);
+        // classico.marcarGol(sobral, jogador5);
+        // classico.gerarResultados();
     }
 }
--- src/Time.java
+++ src/Time.java
@@ -4,6 +4,40 @@ import java.util.List;
 public class Time {
     private String nome;
     private List<Jogador> jogadores;
+    private String corCamisa;
+    
+    
+    public Time() {
+    }
+    
+    public Time(String nome) {
+        this.nome = nome;
+        gerarEscalacao();
+    }
+    
+    private void gerarEscalacao() {
+        this.jogadores = new ArrayList<>();
+        
+        for (int i = 1; i <= 11; i++)
+            jogadores.add(new Jogador("Jog "+i));
+        
+        this.corCamisa = "Preto, vermelho e branco";
+        
+        // Feito pelo copilot - Nossa simulação de delay para acessar essas informações no DB
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println("Sleep was interrupted");
+        }
+    }
+
+    public String getCorCamisa() {
+        return corCamisa;
+    }
+
+    public void setCorCamisa(String corCamisa) {
+        this.corCamisa = corCamisa;
+    }
     
     public String getNome() {
         return nome;
@@ -11,11 +45,6 @@ public class Time {
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public Time(String nome) {
-        this.nome = nome;
-        this.jogadores = new ArrayList<>();
     }
 
     public List<Jogador> getJogadores() {
@@ -28,5 +57,34 @@ public class Time {
 
     public void adicionarJogador(Jogador jogador) {
         jogadores.add(jogador);
+    }
+
+    public Time clone() {
+        Time novo = new Time();
+
+        novo.setCorCamisa(this.corCamisa);
+        novo.setNome(this.nome);
+
+        ArrayList<Jogador> novoTime = new ArrayList<>();
+        for (Jogador jogador : jogadores) {
+            Jogador novoJogador = new Jogador(jogador.getNome());
+            novoTime.add(novoJogador);
+        }
+
+        novo.setJogadores(novoTime);
+
+        return novo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("########## Informações sobre ").append(nome).append("##########").append("\n");
+        builder.append("## Lista de Jogadores: ##").append("\n");
+        for (Jogador jogador : jogadores)
+            builder.append(jogador.getNome()).append("  ");
+        
+        builder.append("\n").append("## Cor da camisa: ").append(corCamisa).append("\n");
+        return builder.toString();
     }
 }
